257f42e869f49ce3312cf27d6996cd1ebe10d661
Bo Wan <sean.b.wan@gmail.com>
2015-02-02 14:51:24 -0500

    add model

---
+++ GoogleFinanceLikedChart/GoogleFinanceLikedChart/DataType.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace FinancialHistoricLineChart
+{
+    public class StockInfo:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public DateTime Date { get; set; }
+        public double ClosePrice { get; set; }
+        public double? Ave20 { get; set; }
+        public double? Ave60 { get; set; }
+        public double? Ave120 { get; set; }
+        public int volumn { get; set; }
+        private Visibility _visibility = Visibility.Hidden;
+        public Visibility PointVisibility
+        {
+            get { return _visibility; }
+            set
+            {
+                _visibility = value;
+                OnPropertyChanged("PointVisibility");
+            }
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if(handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
